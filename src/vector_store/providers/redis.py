d6fb34fea18a9998fadec6aa71fcc8201b650e51
Gladwin Dosunmu <gladwin.dos@hotmail.co.uk>
2024-11-12 10:59:50 +0000

    update reciprocal_rank_fusion

--- src/vector_store/providers/redis.py
+++ src/vector_store/providers/redis.py
@@ -329,19 +329,19 @@ class RedisVectorStore(VectorStoreBase):
         return repository_documents
 
     def reciprocal_rank_fusion(
-        self, results_lists: list[list[Document]], num_results: int, constant: int = 60
+        self, results: list[list[Document]], num_results: int, constant: int = 60
     ) -> list[Document]:
         scores: dict[str, float] = {}
-        for results in results_lists:
-            for rank, doc in enumerate(results):
+        for docs in results:
+            for rank, doc in enumerate(docs):
                 scores[doc.id] = scores.get(doc.id, 0) + 1 / (rank + constant)
 
         sorted_doc_ids = sorted(scores.items(), key=lambda x: x[1], reverse=True)
         top_doc_ids = [doc_id for doc_id, _ in sorted_doc_ids[:num_results]]
 
-        id_to_doc = {doc.id: doc for result in results_lists for doc in result}
+        id_to_doc = {doc.id: doc for result in results for doc in result}
 
-        return [id_to_doc[doc_id] for doc_id in top_doc_ids if doc_id in id_to_doc]
+        return [id_to_doc[doc_id] for doc_id in top_doc_ids]
 
     def search_repository(
         self, name: str, query: str, num_results: int
