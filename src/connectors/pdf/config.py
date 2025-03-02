5c2df8a86333864f86a5976d7994c6709bcba737
Nellow <prabeshpathak002@gmail.com>
2025-03-02 21:02:46 +0545

    pypdf no-depracted

--- src/connectors/pdf/config.py
+++ src/connectors/pdf/config.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from pydantic import field_validator, HttpUrl
+from pydantic import field_validator, AnyUrl
 
 from src.connectors.base.config import BaseConnectorConfig
 from src.connectors.connector_type import ConnectorType
@@ -7,9 +7,14 @@ from src.connectors.connector_type import ConnectorType
 
 class PdfConfig(BaseConnectorConfig):
     type: Literal[ConnectorType.PDF] = ConnectorType.PDF
-    pdf_url: HttpUrl
+    pdf_url: str  
+
     @field_validator("pdf_url")
-    def validate_pdf_url(cls, url: str):
-        if not url.lower().endswith(".pdf"):
-            raise ValueError("URL must end with .pdf")
-        return url
+    def validate_pdf_url(cls, url: str) -> str: 
+        try:
+            parsed_url = AnyUrl(url) 
+            if not parsed_url.path.lower().endswith(".pdf"):
+                raise ValueError("URL must end with .pdf")
+        except ValueError as e:
+            raise ValueError(f"Invalid URL or not a PDF: {e}")
+        return url 
